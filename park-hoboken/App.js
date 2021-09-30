8c71270202bdd2cc6e525d51b2e8192754704e0f
Evan Abel <eabel@stevens.edu>
2021-09-30 16:46:49 -0400

    Added bootstrap button

--- park-hoboken/App.js
+++ park-hoboken/App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button>This is button</Button>
       <StatusBar style="auto" />
     </View>
   );
