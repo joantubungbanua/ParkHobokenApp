5b4583e1747534a2248268276c699824a4db4554
SWiessler <s.wiessler@comcast.net>
2021-10-28 15:56:09 -0400

    refactoring pt 1. no style sheet

---
+++ park-hoboken/config/styles.js
@@ -0,0 +1,33 @@
+const styles = StyleSheet.create({
+    inputView: {
+      backgroundColor: "white",
+      borderRadius: 30,
+      width: "70%",
+      height: 45,
+      marginBottom: 20,
+      alignItems: "center",
+    },
+  
+    TextInput: {
+      height: 50,
+      flex: 1,
+      padding: 10,
+      marginLeft: 20,
+    },
+  
+    forgot_button: {
+      height: 30,
+      marginBottom: 30,
+    },
+  
+    loginBtn:
+    {
+      width: "80%",
+      borderRadius: 25,
+      height: 50,
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 40,
+      backgroundColor: "#FF1493",
+    }
+  });
---
+++ park-hoboken/screens/Profile.js
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { useState } from 'react';
+import {
+  Button,
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  ScrollView
+} from 'react-native';
+
+function ProfileScreen({ navigation }) {
+    const [password, changePassword] = useState('');
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Image 
+          style={{width: 100, height: 100}}
+          source={require('../assets/profile_pic1.png')} />
+  
+        <View style={styles.inputView}>
+          <TextInput
+            placeholder="Change Password"
+            placeholderTextColor="#003f5c"
+            onChangeText={(password) => changePassword(password)}
+          />
+        </View>
+  
+        <Text>Previous Trips:</Text>
+        <View style={{ flex: 1, alignItems: 'left', justifyContent: 'center', borderColor:'gray', borderWidth:3, marginVertical : 10}}>
+            <ScrollView>       
+                <Text>
+                  8th and Wash{"\n"}
+                  9th and Hudson{"\n"}
+                  2nd and Court{"\n"}
+                  1st and Park{"\n"}
+                  3rd and Willow{"\n"}
+                </Text>      
+            </ScrollView>
+        </View>
+        <View style = {{ marginVertical : 10 }}>
+          <Button title="Current Rewards"/>
+        </View>
+        <View style = {{ marginVertical : 10 }}>
+          <Button title="New Trip" onPress={() => navigation.navigate('Selection')} />
+        </View>
+      </View>
+    );
+  }
+
+  export default ProfileScreen;
