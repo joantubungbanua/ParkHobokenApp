2afb9d309544486272134463a9c58f77d5647d7d
Joan Marie Tubungbanua <joantubungbanua@gmail.com>
2021-11-03 16:13:23 -0400

    put firebase config in config.js and not in createaccount.js

--- park-hoboken/screens/CreateAccount.js
+++ park-hoboken/screens/CreateAccount.js
@@ -12,31 +12,7 @@ import {
 } from 'react-native';
 // Stylesheet
 import styles from '../stylesheet.js';
-// Import the functions you need from the SDKs you need
-//import * as firebase from 'firebase/app';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-
-
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAYVnHzBJgamHLd3OqWaWrvDCjKciWSkyo",
-  authDomain: "park-hoboken.firebaseapp.com",
-  databaseURL: "https://park-hoboken-default-rtdb.firebaseio.com",
-  projectId: "park-hoboken",
-  storageBucket: "park-hoboken.appspot.com",
-  messagingSenderId: "19569129787",
-  appId: "1:19569129787:web:f8fcf28b0a08bcca45df1e"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-
+import { firebase } from '../firebase/config'
 
 
 
