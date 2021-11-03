b1877e275172845cf3c75b1d85e1350e291f130d
Joan Marie Tubungbanua <joantubungbanua@gmail.com>
2021-11-02 20:11:12 -0400

    add rough registration logic

--- park-hoboken/firebase/config.js
+++ park-hoboken/firebase/config.js
@@ -1,13 +1,12 @@
 // Import the functions you need from the SDKs you need
-import * as firebase from 'firebase';
-import '@firebase/auth';
-import '@firebase/firestore';
+import * as firebase from 'firebase/app';
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+firebaseConfig = firebase.initializeApp( {
   apiKey: "AIzaSyAYVnHzBJgamHLd3OqWaWrvDCjKciWSkyo",
   authDomain: "park-hoboken.firebaseapp.com",
   databaseURL: "https://park-hoboken-default-rtdb.firebaseio.com",
@@ -15,10 +14,14 @@ const firebaseConfig = {
   storageBucket: "park-hoboken.appspot.com",
   messagingSenderId: "19569129787",
   appId: "1:19569129787:web:f8fcf28b0a08bcca45df1e"
-};
+});
 
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
 
-export { firebase };
+
+//export const db = firebase.firestore();
+//export const storage = firebase.storage();
+
+require('firebase/auth')
+require('firebase/firestore')
+export {firebase};
+
--- park-hoboken/screens/CreateAccount.js
+++ park-hoboken/screens/CreateAccount.js
@@ -12,71 +12,159 @@ import {
 } from 'react-native';
 // Stylesheet
 import styles from '../stylesheet.js';
+// Import the functions you need from the SDKs you need
+//import * as firebase from 'firebase/app';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAYVnHzBJgamHLd3OqWaWrvDCjKciWSkyo",
+  authDomain: "park-hoboken.firebaseapp.com",
+  databaseURL: "https://park-hoboken-default-rtdb.firebaseio.com",
+  projectId: "park-hoboken",
+  storageBucket: "park-hoboken.appspot.com",
+  messagingSenderId: "19569129787",
+  appId: "1:19569129787:web:f8fcf28b0a08bcca45df1e"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
+
+
+
 
 function CreateAccountScreen({ navigation }) {
-    return (
-      <View style={styles.defaultView}>
-        <Text>Enter Profile Details</Text>
-        {/* First name */}
-        <View>
-          <TextInput
-            placeholder="First Name"
-            placeholderTextColor="#003f5c"
-            onChangeText={(first_name) => setFirst_Name(first_name)}
-          />
-        </View>
-        {/* Car Make/Model/Color */}
-        <View>
-          <TextInput
-            placeholder="Vehicle Make"
-            placeholderTextColor="#003f5c"
-            onChangeText={(make) => setFirst_Name(make)}
-          />
-        </View>
-        <View>
-          <TextInput
-            placeholder="Vehicle Model"
-            placeholderTextColor="#003f5c"
-            onChangeText={(model) => setFirst_Name(model)}
-          />
-        </View>
-        <View>
-          <TextInput
-            placeholder="Vehicle Color"
-            placeholderTextColor="#003f5c"
-            onChangeText={(color) => setFirst_Name(color)}
-          />
-        </View>
-        {/* License Plate */}
-        <View>
-          <TextInput
-            placeholder="License Plate"
-            placeholderTextColor="#003f5c"
-            onChangeText={(license) => setFirst_Name(license)}
-          />
-        </View>
-        {/* Email/Password */}
-        <View>
-          <TextInput
-            placeholder="Email"
-            placeholderTextColor="#003f5c"
-            onChangeText={(create_email) => setFirst_Name(create_email)}
-          />
-        </View>
-        <View>
-          <TextInput
-            placeholder="Password"
-            placeholderTextColor="#003f5c"
-            onChangeText={(password) => setFirst_Name(password)}
-          />
-        </View>
+
+
+
+  const [firstName, setFirst_Name] = useState('')
+  const [make, setMake] = useState('')
+  const [model, setModel ] = useState('')
+  const [color, setColor] = useState('')
+  const [license, setLicense] = useState('')
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState('')
+ 
+ 
+
+
+
+  const onRegisterPress = () => {
   
-        <Button
-        title="Create Account"
-        onPress={() => navigation.navigate('Profile')}
-        />
-      </View>
-    );
+
+    firebase.auth().createUserWithEmailAndPassword(email, password).then((response) => {
+        const uid = response.user.uid
+        const data = {
+          id: uid,
+          email,
+          firstName,
+          make,
+          model,
+          color,
+          license,
+          email,
+
+        };
+        const usersRef = firebase.firestore().collection('users')
+        usersRef
+          .doc(uid)
+          .set(data)
+          .then(() => {
+            navigation.navigate('Profile', { user: data })
+          })
+          .catch((error) => {
+            alert(error)
+
+          });
+
+
+
+      })
+      .catch((error) => {
+        alert(error)
+      });
   }
 
-  export default CreateAccountScreen;
+
+  return (
+    <View style={styles.defaultView}>
+      <Text>Enter Profile Details</Text>
+      {/* First name */}
+      <View>
+        <TextInput
+          placeholder="First Name"
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => setFirst_Name(text)}
+          value={firstName}
+        />
+      </View>
+      {/* Car Make/Model/Color */}
+      <View>
+        <TextInput
+          placeholder="Vehicle Make"
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => setMake(text)}
+          value = {make}
+        />
+      </View>
+      <View>
+        <TextInput
+          placeholder="Vehicle Model"
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => setModel(text)}
+          value = {model}
+        />
+      </View>
+      <View>
+        <TextInput
+          placeholder="Vehicle Color"
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => setColor(text)}
+          value = {color}
+        />
+      </View>
+      {/* License Plate */}
+      <View>
+        <TextInput
+          placeholder="License Plate"
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => setLicense(text)}
+          value = {license}
+        />
+      </View>
+      {/* Email/Password */}
+      <View>
+        <TextInput
+          placeholder="Email"
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => setEmail(text)}
+          value = {email}
+      
+        />
+      </View>
+      <View>
+        <TextInput
+          placeholder="Password"
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => setPassword(text)}
+          value = {password}
+        />
+      </View>
+
+      <Button
+        title="Create Account"
+        onPress={() => onRegisterPress()}
+      />
+    </View>
+  );
+}
+
+export default CreateAccountScreen;
