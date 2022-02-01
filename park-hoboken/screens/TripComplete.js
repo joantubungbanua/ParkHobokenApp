ff13f09ebd5d0af0278856d306340eaa896d00aa
Esethral <evanabel311@gmail.com>
2022-02-01 15:56:14 -0500

    fixing merge conflicts

--- park-hoboken/screens/TripComplete.js
+++ park-hoboken/screens/TripComplete.js
@@ -24,23 +24,23 @@ function ratingCompleted(rating) {
 }
 
 function MatchingScreen({ navigation }) {
-  
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-    return (
-      <View style={{height:'100%'}}>
-        <Header
-          leftComponent={{ icon: 'menu', color: '#fff', iconStyle: { color: '#fff' }}}
-          centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
-          rightComponent={{ icon: 'home', color: '#fff', onPress: () => navigation.navigate('Profile') }}
-          containerStyle={{
-            backgroundColor: '#A74F49',
-            justifyContent: 'space-around',
-          }}
-        />
+  return (
+    <View style={{ height: '100%' }}>
+      <Header
+        leftComponent={{ icon: 'menu', color: '#fff', iconStyle: { color: '#fff' } }}
+        centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+        rightComponent={{ icon: 'home', color: '#fff', onPress: () => navigation.navigate('Profile') }}
+        containerStyle={{
+          backgroundColor: '#A74F49',
+          justifyContent: 'space-around',
+        }}
+      />
 
-        <View style={styles.defaultView}>
+      <View style={styles.defaultView}>
 
           <View style={{flex: 1, alignItems: 'center', textAlignVertical: 'top', padding: 10, width: '100%'}}>
             <Text>&nbsp;</Text>
@@ -49,32 +49,32 @@ function MatchingScreen({ navigation }) {
             <TripRewards/>
           </View>
 
-          <View>
-            <Text>Survey questions? Unsure if needed. Maybe just put a check on the routing screen to say they parked/departed</Text>
-          </View>
-
-          <View style={{flex: 1, alignItems: 'center', textAlignVertical: 'bottom', padding: 10, width: '100%'}}>
-            {/* https://www.npmjs.com/package/react-native-ratings */}
-            <Rating
-              type='custom'
-              ratingColor='#A74F49'
-              ratingBackgroundColor='#c8c7c8'
-              ratingCount={5}
-              imageSize={30}
-              style={{ paddingVertical: 10 }}
-            />
-            <Text>Feedback:</Text>
-            <View style={styles.inputView}>
-              <TextInput 
-                placeholder="Feedback"
-              />
-            </View>
-          </View>
-
-
+        <View>
+          <Text>Survey questions? Unsure if needed. Maybe just put a check on the routing screen to say they parked/departed</Text>
         </View>
+
+        <View style={{ flex: 1, alignItems: 'center', textAlignVertical: 'bottom', padding: 10, width: '100%' }}>
+          {/* https://www.npmjs.com/package/react-native-ratings */}
+          <Rating
+            type='custom'
+            ratingColor='#A74F49'
+            ratingBackgroundColor='#c8c7c8'
+            ratingCount={5}
+            imageSize={30}
+            style={{ paddingVertical: 10 }}
+          />
+          <Text>Feedback:</Text>
+          <View style={styles.inputView}>
+            <TextInput
+              placeholder="Feedback"
+            />
+          </View>
+        </View>
+
+
       </View>
-    );
+    </View>
+  );
 }
 
 export default MatchingScreen;
